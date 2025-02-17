f60cb5ad0a3cc823b1467f8a5916b6fb84b29534
Dev205 <dev205mg@gmail.com>
2025-02-17 16:43:09 +0300

    add page in load

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+import { alertElement } from "./alert.js";
 import { fetchJSON } from "./functions/api.js";
 
 class InfinitePagination {
-
   /**@type {string} */
   #endpoint;
   /**@type {HTMLTemplateElement} */
   #template;
   /**@type {HTMLElement} */
   #target;
+  /**@type {HTMLElement} */
+  #loader;
   /**@type {object} */
   #elements;
   /**@type {IntersectionObserver} */
@@ -18,44 +20,64 @@ class InfinitePagination {
   #page = 1;
 
   /**
-   * 
-   * @param {HTMLElement} element 
+   *
+   * @param {HTMLElement} element
    */
-  constructor (element){
+  constructor(element) {
+    this.#loader = element;
     this.#endpoint = element.dataset.endpoint;
     this.#template = document.querySelector(element.dataset.template);
     this.#target = document.querySelector(element.dataset.target);
     this.#elements = JSON.parse(element.dataset.elements);
     this.#observer = new IntersectionObserver((entries) => {
-      for(const entrie of entries){
-        if(entrie.isIntersecting){
+      for (const entrie of entries) {
+        if (entrie.isIntersecting) {
           this.#loadMore();
         }
       }
-    })
+    });
     this.#observer.observe(element);
   }
 
-
-  async #loadMore(){
-    if(this.#loading){
+  async #loadMore() {
+    if (this.#loading) {
       return;
     }
     this.#loading = true;
-    const commnets = await fetchJSON(this.#endpoint);
-    for(const comment of commnets){
-      const commentElement = this.#template.content.cloneNode(true);
-      for(const [key, selector] of Object.entries(this.#elements)){
-        commentElement.querySelector(selector).innerText = comment[key];
+    try {
+      const url = new URL(this.#endpoint);
+      url.searchParams.set("_page", this.#page);
+      const commnets = await fetchJSON(url.toString());
+      if (commnets.length === 0) {
+        this.#stopObserving();
+        return;
       }
-      this.#target.append(commentElement);
+      for (const comment of commnets) {
+        const commentElement = this.#template.content.cloneNode(true);
+        for (const [key, selector] of Object.entries(this.#elements)) {
+          commentElement.querySelector(selector).innerText = comment[key];
+        }
+        this.#target.append(commentElement);
+      }
+      this.#page++;
+      this.#loading = false;
+    } catch (e) {
+      this.#loader.style.display = "none";
+      const error = alertElement("Impossible de charger les contenus");
+      error.addEventListener("close", () => {
+        this.#loader.style.removeProperty("display");
+        this.#loading = false;
+      });
+      this.#target.append(error);
     }
-    this.#loading = false;
   }
 
-
+  #stopObserving() {
+    this.#observer.disconnect();
+    this.#loader.remove();
+  }
 }
 
 document
-  .querySelectorAll('.js-infinite-pagination')
-  .forEach( el => new InfinitePagination(el));
+  .querySelectorAll(".js-infinite-pagination")
+  .forEach((el) => new InfinitePagination(el));
